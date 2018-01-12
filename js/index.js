10dd3f3fc8d63bc2358fdcfedbebb64f645ef6cd
zhouXA <379498615@qq.com>
2018-01-13 00:11:30 +0800

    小试牛刀

---
+++ js/index.js
@@ -0,0 +1,10 @@
+;(function(){
+    var mySwiper = new Swiper('.swiper-container',{
+        loop:true,
+        autoplay:{
+            delay:1000,
+            disableOnInteraction:false
+        },
+        effect: "slide"
+    })
+})()
